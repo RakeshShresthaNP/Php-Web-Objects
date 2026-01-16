62080e783c649a5d0a4d17e2304568ce86a1b2ab
Rakesh Shrestha <rakesh_shresthanp@yahoo.com>
2026-01-16 12:37:46 +0545

    Update config.php

--- myapp/app/config/config.php
+++ myapp/app/config/config.php
@@ -65,14 +65,11 @@ ini_set('expose_php', '0');
 // Strict timeout: Kill any script taking longer than 10 seconds
 set_time_limit(10);
 
-// Limit the memory a single script can use (e.g., 64 Megabytes)
-ini_set('memory_limit', '64M');
+// Limit the memory a single script can use (e.g., 128 Megabytes)
+ini_set('memory_limit', '128M');
 
 // Ensure database/file writes finish even if the user closes their tab
 ignore_user_abort(true);
 
 // Limit how long PHP spends parsing file uploads
 ini_set('max_input_time', '60');
-
-
-
