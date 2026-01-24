276441e67e6e9a63e64795a547bf84b3e7d5a853
Rakesh Shrestha <rakesh_shresthanp@yahoo.com>
2026-01-24 22:16:30 +0545

    updates headers

--- myapp/app/controllersws/chat.php
+++ myapp/app/controllersws/chat.php
@@ -3,6 +3,12 @@
 /**
  # Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
  # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions are
+ # met:
+ #
+ # Redistributions must retain the above copyright notice.
  */
 declare(strict_types = 1);
 
